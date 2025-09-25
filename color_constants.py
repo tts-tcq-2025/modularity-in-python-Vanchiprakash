b18651e5df4081396c775ade9b1a29773d9834c8
Vanchiprakash <vanchi110@gmail.com>
2025-09-25 09:39:52 +0530

    Create color_constants.py

---
+++ color_constants.py
@@ -0,0 +1,5 @@
+# List of major colors used in color coding
+MAJOR_COLORS = ['White', 'Red', 'Black', 'Yellow', 'Violet']
+
+# List of minor colors used in color coding
+MINOR_COLORS = ["Blue", "Orange", "Green", "Brown", "Slate"]
